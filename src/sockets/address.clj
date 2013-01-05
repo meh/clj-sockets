9f1e52bec722786c488b7cbe430fc05b21db3fe5
meh <meh@schizofreni.co>
2013-01-05 20:08:40 +0100

    address: add base implementation

---
+++ src/sockets/address.clj
@@ -0,0 +1,71 @@
+;; Copyleft (ɔ) meh. - http://meh.schizofreni.co
+;;
+;; This file is part of clj-sockets - https://github.com/meh/clj-sockets
+;;
+;; clj-sockets is free software: you can redistribute it and/or modify it under
+;; the terms of the Lesser GNU General Public License as published by the Free
+;; Software Foundation, either version 3 of the License, or (at your option)
+;; any later version.
+;;
+;; clj-sockets is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE. See the Lesser GNU General Public License
+;; for more details.
+;;
+;; You should have received a copy of the Lesser GNU General Public License
+;; along with clj-sockets If not, see <http://www.gnu.org/licenses/>.
+
+(ns sockets.address
+  (:require [sockets.native :as n]))
+
+(defprotocol Address
+  (domain [this])
+  (native [this]))
+
+(deftype InternetAddress [address port]
+  Address
+  (domain [this]
+    :inet)
+  (native [this]
+    (n/to-sockaddr :inet address port))
+
+  Object
+  (toString [this]
+    (str address ":" port)))
+
+(deftype Internet6Address [address port flow-info scope-id]
+  Address
+  (native [this]
+    (n/to-sockaddr :inet6 address port flow-info scope-id))
+  (domain [this]
+    :inet6)
+
+  Object
+  (toString [this]
+    (str "[" address "]:" port)))
+
+(deftype UNIXAddress [path]
+  Address
+  (native [this]
+    (n/to-sockaddr :unix path))
+  (domain [this]
+    :unix)
+
+  Object
+  (toString [this]
+    (str path)))
+
+(defn native->InternetAddress [ptr]
+  (let [from (n/from-sockaddr :inet ptr)]
+    from))
+
+(defn native->Internet6Address [ptr]
+  (let [from (n/from-sockaddr :inet6 ptr)]
+    from))
+
+(defn native->UNIXAddress [ptr]
+  (let [from (n/from-sockaddr :unix ptr)]
+    from))
+
+(defn internet? [addr]
+  (or (instance? InternetAddress addr) (instance? Internet6Address addr)))
