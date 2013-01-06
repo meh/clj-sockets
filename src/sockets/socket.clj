e9e24545e1e80fc561c9d609f4763b602d098e19
meh <meh@schizofreni.co>
2013-01-06 03:15:09 +0100

    socket: improve Sendable

--- src/sockets/socket.clj
+++ src/sockets/socket.clj
@@ -17,7 +17,8 @@
 
 (ns sockets.socket
   (:refer-clojure :exclude [send set get])
-  (:import [java.nio ByteBuffer ByteOrder]))
+  (:require [sockets.native :as n])
+  (:import [java.nio ByteBuffer CharBuffer ShortBuffer IntBuffer LongBuffer]))
 
 (defprotocol Socket
   (connect [this addr])
@@ -30,10 +31,51 @@
   (send [this data])
   (recvfrom [this size])
   (sendto [this to data])
-  (set [this option data] [this level option data])
-  (get [this option] [this level option])
-  (local [this])
+  (set [this option] [this option data])
+  (unset [this option])
+  (get [this option])
+  (local-address [this])
   (remote [this]))
+
+(defonce ^:private options
+  (case (System/getProperty "os.name")
+    "Linux" {:broadcast    [6  :bool]
+             :reuse-addr   [2  :bool]
+             :keep-alive   [9  :bool]
+             :linger       [13 :bool]
+             :do-not-route [5  :bool]}
+
+    "FreeBSD" {:broadcast    [0x20 :bool]
+               :reuse-addr   [0x04 :bool]
+               :keep-alive   [0x08 :bool]
+               :linger       [0x80 :bool]
+               :do-not-route [0x10 :bool]}
+
+    ("Mac OS" "Mac OS X") {:broadcast    [0x20 :bool]
+                           :reuse-addr   [0x04 :bool]
+                           :keep-alive   [0x08 :bool]
+                           :linger       [0x80 :bool]
+                           :do-not-route [0x10 :bool]}))
+
+(defonce ^:private socket-level
+  (case (System/getProperty "os.name")
+    "Linux"               1
+    "FreeBSD"             0xffff
+    ("Mac OS" "Mac OS X") 0xffff))
+
+(defn handles-option? [name]
+  (contains? options name))
+
+(defn set-option [socket name & data]
+  (let [[id type] (options name)]
+    (apply n/setsockopt (.fd socket) socket-level id
+           (case type
+             :bool [(n/pointer-for :bool (or (first data) true)) (n/size-for :bool)]))))
+
+(defn get-option [socket name]
+  (let [[id type] (options name)]
+    (case type
+      :bool (n/getsockopt (.fd socket) socket-level (n/pointer-for :int) (n/size-for :int)))))
 
 (defprotocol Sendable
   (sendable [this]))
@@ -41,17 +83,41 @@
 (extend-protocol Sendable
   Byte
   (sendable [this]
-    (.put (ByteBuffer/allocate Byte/SIZE) this))
+    (let [size Byte/SIZE]
+      [(.put (ByteBuffer/allocateDirect size) this) size]))
 
   Short
   (sendable [this]
-    (.putShort (ByteBuffer/allocate Short/SIZE) this))
+    (let [size Short/SIZE]
+      [(.putShort (ByteBuffer/allocateDirect size) this) size]))
 
   Integer
   (sendable [this]
-    (.putInt (ByteBuffer/allocate Integer/SIZE) this))
+    (let [size Integer/SIZE]
+      [(.putInt (ByteBuffer/allocateDirect size) this) size]))
 
   Long
   (sendable [this]
-    (.putLong (ByteBuffer/allocate Long/SIZE) this)))
+    (let [size Long/SIZE]
+      (.putLong (ByteBuffer/allocateDirect Long/SIZE) this)))
 
+  String
+  (sendable [this]
+    (let [buf (.getBytes this "UTF-8")]
+      [buf (alength buf)]))
+
+  ByteBuffer
+  (sendable [this]
+    [this (.position this)])
+
+  ShortBuffer
+  (sendable [this]
+    [this (* (.position this) Short/SIZE)])
+
+  IntBuffer
+  (sendable [this]
+    [this (* (.position this) Integer/SIZE)])
+
+  LongBuffer
+  (sendable [this]
+    [this (* (.position this) Long/SIZE)]))
